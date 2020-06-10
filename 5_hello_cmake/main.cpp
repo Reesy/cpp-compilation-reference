6ad2e9b3e9f389acb5227d4d8dade520b9ded80f
James Rees <James.Rees@outlook.com>
2020-06-10 23:09:07 +0100

    Added cmake build project to show the syntax

---
+++ 5_hello_cmake/main.cpp
@@ -0,0 +1,8 @@
+#include "hello.hpp"
+
+// Main function that will run call the function from hello.cpp, now defined in hello.hpp then terminate.
+int main()
+{
+    printHelloWorld();
+    return 0;
+}
