0aa8a7846a4db75312392389c2f5932dab5a3d69
James Rees <James.Rees@outlook.com>
2020-06-12 16:52:50 +0100

    Added initial file structure around example library creation

---
+++ 6_hello_custom_library/include/badmath.hpp
@@ -0,0 +1,6 @@
+#ifndef BADMATH_HPP
+#define BADMATH_HPP
+
+int add(int op1, int op2); 
+
+#endif
---
+++ 6_hello_custom_library/src/badmath.cpp
@@ -0,0 +1,9 @@
+#include "badmath.hpp"
+
+/*
+    This actually does subtraction.
+*/
+int add(int op1, int op2)
+{
+    return op1 - op2;
+};
